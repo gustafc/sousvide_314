d55b5415bed916068c6e7b1afab0e9be81159fed
Gustaf Cele <gustafc@gmail.com>
2014-11-15 14:37:16 +0100

    Dist scripts

---
+++ scripts/build-tarball.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+
+cd $(dirname "$0")/..
+
+mkdir -p build
+git ls-files | xargs tar cJf build/sv314.tar.xz
---
+++ scripts/install-to-pi.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+PI_IP=$1
+test -z "$1" && echo "Usage: $0 pi_hostname_or_ip" && exit -1
+cd $(dirname "$0")/..
+./scripts/build-tarball.sh
+cat build/sv314.tar.xz | ssh pi@"$PI_IP" 'sh -c "mkdir -p sv314  && cd sv314  && tar xJv"'
