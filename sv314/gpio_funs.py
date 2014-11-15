0da7063e210b3eb46d1a673b387a6261080d3470
Gustaf Cele <gustaf.cele@mejsla.se>
2014-11-15 17:45:04 +0000

    Working sous vide!

--- sv314/gpio_funs.py
+++ sv314/gpio_funs.py
@@ -1,7 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
-PIN=7
+_turned_on = True
+_target_temperature = 54.5
+
+PIN=11
+filename='/mnt/1wire/28.383E84050000/temperature'
 
 def gpio_setup():
   GPIO.setmode(GPIO.BOARD)
@@ -16,12 +20,28 @@ def gpio_off():
 def _gpio_switch(on):
   GPIO.output(PIN, on)
 
+def read_file():
+  with file(filename) as f:
+      return float(f.read())
+
+def set_temperature(new_temp):
+  _target_temperature = new_temp
 
 if __name__ == '__main__':
   gpio_setup()
-  n=0
-  while True:
-    n+=1
-    _gpio_switch(n % 2 == 0)
-    time.sleep(1)
-
+  try:
+    is_heating=False
+    while True:
+      temp = read_file()
+      should_heat = _turned_on and temp < _target_temperature
+      print temp, should_heat
+      if should_heat and not is_heating:
+        gpio_on()
+        is_heating = True
+      elif not should_heat and is_heating:
+        gpio_off()
+        is_heating = False
+      time.sleep(1)
+  finally: 
+    GPIO.cleanup()
+    
